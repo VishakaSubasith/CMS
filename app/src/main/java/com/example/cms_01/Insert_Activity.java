d9821636efb89690ac3ff2f93e918c7dbc43ea4e
Charuka <charuka2708487@gmail.com>
2019-08-02 16:58:20 +0530

    third member done

--- app/src/main/java/com/example/cms_01/Insert_Activity.java
+++ app/src/main/java/com/example/cms_01/Insert_Activity.java
@@ -11,7 +11,7 @@ public class Insert_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_);
         setTitle(R.string.lable4);
-       // Spinner spinner = findViewById(R.id.spinner1);
+       //Spinner spinner = findViewById(R.id.spinner1);
 
         //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.numbers, android.R.layout.simple_spinner_item);
         //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
