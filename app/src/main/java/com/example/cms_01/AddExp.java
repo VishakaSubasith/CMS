559a75426f8582fa04dee9771e27f9568fc15f1f
vishakasubasith <subasith@gmail.com>
2019-08-02 15:43:13 +0530

    Update Exense - anuj

--- app/src/main/java/com/example/cms_01/AddExp.java
+++ app/src/main/java/com/example/cms_01/AddExp.java
@@ -3,6 +3,7 @@ package com.example.cms_01;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -11,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class AddExp extends AppCompatActivity {
 ImageView img1;
 ListView listview1;
+Button expUpdate;
+Button expDelete;
 
 
     @Override
@@ -19,6 +22,10 @@ ListView listview1;
         setContentView(R.layout.activity_add_exp);
 
         img1 = (ImageView) findViewById(R.id.imageView3);
+        expUpdate = (Button) findViewById(R.id.expUpdate);
+        expDelete = (Button) findViewById(R.id.expDelete);
+
+
 
         // Capture button clicks
         img1.setOnClickListener(new View.OnClickListener() {
@@ -27,10 +34,38 @@ ListView listview1;
                 openAddExp();
             }
         });
+
+        expUpdate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                ExpUpdate();
+            }
+        });
+
+        expDelete.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                ExpUpdate();
+            }
+        });
+
     }
 
     public void openAddExp(){
         Intent intent1 = new Intent(this,ExpMng.class);
+        startActivity(intent1);
+
+    }
+
+
+    public void ExpUpdate(){
+        Intent intent1 = new Intent(this,ExpUpdate.class);
+        startActivity(intent1);
+
+    }
+
+    public void ExpDelete(){
+        Intent intent1 = new Intent(this,ExpUpdate.class);
         startActivity(intent1);
 
     }
